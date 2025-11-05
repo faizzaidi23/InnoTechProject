64262f3b88f6520089aeaba92ca90ae604f68138
faizzaidi23 <786faizzaidi@gmail.com>
2025-11-05 12:30:12 +0530

    feat:changed the sdk version from 35 to 36 to make all the dependencies work completely

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.innotechproject"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.innotechproject"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
